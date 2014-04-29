762b807d1243cb523b93808737fb0f301f469aeb
taketin <tksthdnr@gmail.com>
2014-04-29 22:02:06 +0900

    add menupop app

---
+++ Casks/menupop.rb
@@ -0,0 +1,7 @@
+class MenuPop < Cask
+  url 'http://www.binarybakery.com/products/MenuPop.dmg'
+  homepage 'http://www.binarybakery.com/aprod/index.html'
+  version 'latest'
+  sha256 'bc55309f60324d046756839de6f40b2271c79cf3'
+  link 'MenuPop.app'
+end
