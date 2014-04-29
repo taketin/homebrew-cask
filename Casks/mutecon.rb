416153f36489fb091d3dab564c2cc8824f4bcfc7
taketin <tksthdnr@gmail.com>
2014-04-29 22:10:25 +0900

    add mutecon app

---
+++ Casks/mutecon.rb
@@ -0,0 +1,7 @@
+class MuteCon < Cask
+  url 'http://www.sara-mac.com/fw/mutec202.dmg'
+  homepage 'http://homepage1.nifty.com/macbs/download.htm#MuteCon'
+  version 'latest'
+  sha256 '668c97f5488e19afdffd4fdf4100c9f7343175cd'
+  link 'MuteCon.app'
+end
